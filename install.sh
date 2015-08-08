32e2f98afb48419d0933d914e2f02f7dcce12885
Elias Probst <mail@eliasprobst.eu>
2015-08-09 00:25:29 +0200

    Never use '/usr' as default prefix for manual installation scripts
    Usign `/usr` as PREFIX for an installation script which is intended to be executed by a user is a very bad practice - `/usr` is the package manager's realm.
    
    Every manually built/managed file should go somewhere else (e.g. `/usr/local`).

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 cd ./tmp
-cmake ../ -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DLIB_INSTALL_DIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
+cmake ../ -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release -DLIB_INSTALL_DIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
 make
 sudo make install
 cd ../
